ff4d9d50218f7fb1359797ffcf321375a4093b5e
Fardeen Faisal <fardeenfaisal.fs@gmail.com>
2023-03-10 16:07:43 -0700

    count pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -57,7 +57,7 @@ public class CustomList extends ArrayAdapter<City> {
         return cities.contains(city);
     }
     public int countCities() {
-        return 0;
+        return cities.size();
     }
 
 
